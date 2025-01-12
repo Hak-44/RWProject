e84f14b67fbbd6164b8df7781aac5c83bd1a5a7c
Hak-44 <hak.braithwaite@outlook.com>
2025-01-12 02:17:34 +0000

    Added a raycast for the webpage, which allows mouse tracking, which then changes the colour of the line when hovering over it. Also Moving around code and added comments.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; // orbital controls allow movement of the camera, changing the perspective. 
 
 var wallCount = 0;
 
@@ -19,7 +19,7 @@ camera.position.set( 0, 0, 100 );
 
 // orbiting controls allow the user to move around the 3D space created. Reference: https://threejs.org/docs/#examples/en/controls/OrbitControls
 const controls = new OrbitControls( camera, renderer.domElement );
-camera.lookAt( 0, 0, 0 );
+//camera.lookAt( 0, 0, 0 );
 // interita factors contribute to the smoothness of the camera movement controls. 
 controls.enableDamping = true;  // enables the inertia
 controls.dampingFactor = 0.1;   // sets the inerita value
@@ -28,9 +28,14 @@ controls.dampingFactor = 0.1;   // sets the inerita value
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x36454F ); // background colour of 3D env
 
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+
 
 // here is used to loop the scene so it keeps getting updated (siilar to update() in unity)
 function animate() {
+    MouseRaycast();
     controls.update();  // updating the controls from the camera
 	renderer.render( scene, camera );   // final render of the scene 
 }
@@ -43,6 +48,44 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
+
+var selectedObject = null;   /* this object is recorded whenever the mosue is hovered over, that way 
+    when the object*/
+var lines;
+
+/* Raycasting is the used for tracking the mouse picking and can be used to check if users are 
+hovering over 3D objects within the space. 
+
+ref:https://threejs.org/docs/index.html?q=ray#api/en/core/Raycaster
+
+*/
+window.addEventListener( 'pointermove', onPointerMove );
+
+function onPointerMove(event){
+    pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function MouseRaycast(){
+    raycaster.setFromCamera( pointer, camera );
+
+	/* calculate objects intersecting the picking ray (needs to be var since it will be constantly updated)
+        which will return an array of objects  */
+	var intersects = raycaster.intersectObjects( scene.children );
+
+    if(intersects.length > 0){
+        selectedObject = intersects[0].object;
+        selectedObject.material.color.set( 0xFFE59B );
+    }else{
+        if(selectedObject){
+            selectedObject.material.color.set( 0xFFFFFF );
+            selectedObject = null;
+        }
+    }
+    
+
+	
+}
 
 document.getElementById('createRoomDiv').addEventListener('click', doSomething);
 function doSomething(){
@@ -58,18 +101,14 @@ function doSomething(){
     // data that represents mesh, lines or point geometry data.
     const geometry = new THREE.BufferGeometry().setFromPoints( points );    
 
-    points[0].set(1, 5, 5);
-    points[1].set(1, 5, 90);
+    points[0].set(1, 0, 5);
+    points[1].set(1, 0, 90);
     geometry.setFromPoints(points);
 
-    const line = new THREE.Line( geometry, material );
+    lines = new THREE.Line( geometry, material );
 
-    scene.add( line );
+    scene.add( lines );
 }
-
-
-
-
 
 
 
