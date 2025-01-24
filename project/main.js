95ee39b3f67430326e763ebc381598e1f46c2b55
Hak-44 <hak.braithwaite@outlook.com>
2025-01-24 02:58:00 +0000

    Adding another perspective camera to the scene. Renaming the original to orbital camera and the new camera to skyCamera. They change according to the mainCamera var variable.

--- project/main.js
+++ project/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; // orbital controls allow movement of the camera, changing the perspective. 
+import { mx_bilerp_0 } from 'three/src/nodes/materialx/lib/mx_noise.js';
 
 var wallCount = 0;
 
@@ -13,17 +14,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// camera setup for the scene
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 0, 0, 100 );
-
-// orbiting controls allow the user to move around the 3D space created. Reference: https://threejs.org/docs/#examples/en/controls/OrbitControls
-const controls = new OrbitControls( camera, renderer.domElement );
-//camera.lookAt( 0, 0, 0 );
-// interita factors contribute to the smoothness of the camera movement controls. 
-controls.enableDamping = true;  // enables the inertia
-controls.dampingFactor = 0.1;   // sets the inerita value
-
 // the scene void that is visible
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xf8f8f8 ); // background colour of 3D env
@@ -32,20 +22,48 @@ scene.background = new THREE.Color( 0xf8f8f8 ); // background colour of 3D env
 scene.fog = new THREE.Fog(0xD3D3D3, 0.0025, 500);
 
 
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-
+// ground properties
 const groundGeometry = new THREE.PlaneGeometry(10000, 10000)
 const groundMaterial = new THREE.MeshBasicMaterial({
     color: 0xE5E4E2
     
 })
 
+// plane rendering.
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial)
 groundMesh.position.set(0, -2, 0)   // positioning at -2 places it downwards from starting view
 groundMesh.rotation.set(Math.PI / -2, 0, 0) // rotating to be flat on screen
 groundMesh.receiveShadow = true  // giving shadow to the object
 scene.add(groundMesh)
+
+
+// 3D orbiting camera setup for the scene
+const orbitCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+orbitCamera.position.set( 0, 0, 100 );
+scene.add(orbitCamera);
+
+// adding sky camera for the overhead perspective
+const skyCamera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+skyCamera.position.set(1,100,1);    // the sky camera will be set above the plane from a reasonable distance
+skyCamera.lookAt(groundMesh.position);        // the camera is set to the same position as the plane location.
+skyCamera.name = "SkyCam";          // name for easy access
+scene.add(skyCamera);
+
+var currentCamera = orbitCamera;    // set camera is the 
+
+// orbiting controls allow the user to move around the 3D space created. Reference: https://threejs.org/docs/#examples/en/controls/OrbitControls
+const controls = new OrbitControls( orbitCamera, renderer.domElement );
+var is2D = false;   // the variable is checking whether the perspective of the 
+
+
+//camera.lookAt( 0, 0, 0 );
+// interita factors contribute to the smoothness of the camera movement controls. 
+controls.enableDamping = true;  // enables the inertia
+controls.dampingFactor = 0.1;   // sets the inerita value
+
+
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
 
 
@@ -54,15 +72,15 @@ function animate() {
     MouseRaycast();
     CameraLeveling();
     controls.update();  // updating the controls from the camera
-	renderer.render( scene, camera );   // final render of the scene 
+	renderer.render( scene, currentCamera );   // final render of the scene 
 }
 renderer.setAnimationLoop( animate );
 
 // here keeps the scene updated depending on the windows resizing. 
 window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    currentCamera.aspect = window.innerWidth / window.innerHeight;
+    currentCamera.updateProjectionMatrix();
 });
 
 
@@ -86,7 +104,7 @@ function onPointerMove(event){
 }
 
 function MouseRaycast(){
-    raycaster.setFromCamera( pointer, camera );
+    raycaster.setFromCamera( pointer, currentCamera );
 
 	/* calculate objects intersecting the picking ray (needs to be var since it will be constantly updated)
         which will return an array of objects  */
@@ -110,10 +128,27 @@ function MouseRaycast(){
 	
 }
 
+// the div will trigger the change in perspective, it will check the current view and will swap to the other accordingly. 
+document.getElementById('changeCameraPerspectiveDiv').addEventListener('click', changeCamPerspective);
+function changeCamPerspective(){
+    if(!is2D){
+        is2D = true;
+        currentCamera = skyCamera;
+    }else{
+        is2D = false;
+        currentCamera = orbitCamera;
+    }
+
+}
+
+
 /* this camera leveling makes sure the camera will not go beneath the plane, that way it is easier to track the
-  scene. */
+  scene.  */
 function CameraLeveling(){
-    if(camera.position.y < 0) camera.position.y = 0;
+
+    if(!is2D)
+        if(currentCamera.position.y < 0) currentCamera.position.y = 0;
+  
 }
 
 document.getElementById('createRoomDiv').addEventListener('click', doSomething);
